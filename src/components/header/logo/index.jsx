88f68efffd953f8c890341fa7568dba715d472a3
fernando <luizfernandosr@outlook.com.br>
2024-12-02 22:58:35 -0300

    css pra styled components--teste2

--- src/components/header/logo/index.jsx
+++ src/components/header/logo/index.jsx
@@ -1,9 +1,17 @@
-import sQpetsLogo from '/imagens-svg/sQpets.svg'
-import './logo.css'
-
+import sQpetsLogo from '/imagens-svg/sQpets.svg';
+import styled from 'styled-components';
 
 export default function Logo() {
-    return (
-            <img src={sQpetsLogo} className="logo" alt="logo"/>
-    );
+  return <LogoImage src={sQpetsLogo} alt="logo"/>
 }
+
+const LogoImage = styled.img`
+  height: 4rem;
+  width: 13rem;
+  padding: 0.5rem;
+  object-fit: contain;
+
+  @media screen and (max-width: 768px) {
+    width: 40%;
+  }
+`
